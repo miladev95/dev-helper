3fc1e3f787ce9d9c1f5102ac77ad6d7aa8886006
miladev95 <miladev95@gmail.com>
2025-10-08 13:26:18 +0330

    init.sh

--- init.sh
+++ init.sh
@@ -1,18 +1,35 @@
 # init ubuntu system
 
-# ubuntu apps
+# config
+gsettings set org.gnome.shell.extensions.dash-to-dock dock-position 'BOTTOM'
+gsettings set org.gnome.shell favorite-apps "['org.gnome.Nautilus.desktop', 'org.gnome.Terminal.desktop']"
+dconf load / < ./gnome_settings.txt # copy keyboard shortcuts and settings
+
+# apt
 sudo apt update
-sudo snap install tmux
 sudo apt install -y indicator-multiload
+sudo apt install -y thunar
+sudo apt install -y htop
+sudo apt install -y git
+
+# snap
+sudo snap install tmux
 sudo snap install telegram-desktop
 sudo snap install audious
-sudo apt install -y thunar
 sudo snap install curl
-sudo apt install -y htop
+sudo snap install postman
+
+# docker
+sudo snap install docker
+sudo apt install docker-compose-plugin -y
+
+# python
+sudo apt install python3 -y
+sudo snap install pycharm-professional --classic
 
 # golang
+sudo snap install go --classic
 sudo snap install goland --classic
-sudo snap install golang --classic
 
 # php
 sudo apt install -y php php-cli php-fpm php-mysql php-xml php-mbstring php-curl
@@ -21,3 +38,7 @@ sudo snap install phpstorm --classic
 # chrome
 wget -q -O /tmp/google-chrome.deb https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo dpkg -i /tmp/google-chrome.deb
+
+# config
+git config --global core.editor "vim"
+cat bashrc >> ~/.bashrc
