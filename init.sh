994367c0e5ab8ec28cd5875fbd2968f113ba633b
miladev95 <miladev95@gmail.com>
2025-10-08 16:00:37 +0330

    pin

--- init.sh
+++ init.sh
@@ -2,8 +2,18 @@
 
 # config
 gsettings set org.gnome.shell.extensions.dash-to-dock dock-position 'BOTTOM'
-gsettings set org.gnome.shell favorite-apps "['org.gnome.Nautilus.desktop', 'org.gnome.Terminal.desktop']"
+gsettings set org.gnome.shell favorite-apps "['org.gnome.Nautilus.desktop', 'org.gnome.Terminal.desktop','org.gnome.TextEditor.desktop']"
 dconf load / < ./gnome_settings.txt # copy keyboard shortcuts and settings
+
+# chrome
+wget -q -O /tmp/google-chrome.deb https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo dpkg -i /tmp/google-chrome.deb
+gsettings set org.gnome.shell favorite-apps "[
+  'org.gnome.Nautilus.desktop',
+  'org.gnome.Terminal.desktop',
+  'org.gnome.TextEditor.desktop',
+  'google-chrome.desktop'
+]"
 
 # apt
 sudo apt update
@@ -26,18 +36,39 @@ sudo apt install docker-compose-plugin -y
 # python
 sudo apt install python3 -y
 sudo snap install pycharm-professional --classic
+gsettings set org.gnome.shell favorite-apps "[
+  'org.gnome.Nautilus.desktop',
+  'org.gnome.Terminal.desktop',
+  'pycharm-professional_pycharm-professional.desktop',
+  'org.gnome.TextEditor.desktop',
+  'google-chrome.desktop'
+]"
 
 # golang
 sudo snap install go --classic
 sudo snap install goland --classic
+gsettings set org.gnome.shell favorite-apps "[
+  'org.gnome.Nautilus.desktop',
+  'org.gnome.Terminal.desktop',
+  'goland_goland.desktop',
+  'phpstorm_phpstorm.desktop',
+  'pycharm-professional_pycharm-professional.desktop',
+  'org.gnome.TextEditor.desktop',
+  'google-chrome.desktop'
+]"
 
 # php
 sudo apt install -y php php-cli php-fpm php-mysql php-xml php-mbstring php-curl
 sudo snap install phpstorm --classic
-
-# chrome
-wget -q -O /tmp/google-chrome.deb https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-sudo dpkg -i /tmp/google-chrome.deb
+gsettings set org.gnome.shell favorite-apps "[
+  'org.gnome.Nautilus.desktop',
+  'org.gnome.Terminal.desktop',
+  'goland_goland.desktop',
+  'phpstorm_phpstorm.desktop',
+  'pycharm-professional_pycharm-professional.desktop',
+  'org.gnome.TextEditor.desktop',
+  'google-chrome.desktop'
+]"
 
 # config
 git config --global core.editor "vim"
